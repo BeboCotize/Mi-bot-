7d8a91b63ee859f6d1e349c962e03c8468baf6c5
BeboCotize <djmalindo25@hotmail.com>
2025-08-26 19:22:05 -0400

    Actualizar generator.py

--- generator.py
+++ generator.py
@@ -3,96 +3,80 @@ import pytz
 import datetime
 import re
 
-
-# --- Algoritmo de Luhn ---
+# --- Algoritmo Luhn ---
 def luhn_checksum(card_number):
     def digits_of(n):
         return [int(d) for d in str(n)]
     digits = digits_of(card_number)
     odd_digits = digits[-1::-2]
     even_digits = digits[-2::-2]
-    checksum = sum(odd_digits)
+    checksum = 0
+    checksum += sum(odd_digits)
     for d in even_digits:
         checksum += sum(digits_of(d * 2))
     return checksum % 10
-
 
 def is_luhn_valid(card_number) -> bool:
     return luhn_checksum(card_number) == 0
 
 
 # --- Generador de tarjetas ---
-def cc_gen(cc, mes="xx", ano="xxxx", cvv="rnd"):
+def cc_gen(cc, mes='xx', ano='xxxx', cvv='rnd'):
     ccs = []
-    while len(ccs) < 10:  # Genera 10 por defecto
+    while len(ccs) < 10:  # Genera 10 tarjetas por defecto
         card = str(cc)
-        digits = "0123456789"
-        list_digits = list(digits)
-        random.shuffle(list_digits)
-        string_digits = "".join(list_digits)
-        card = card + string_digits
-        new_list = list(card)
-        list_emty = []
 
-        for i in new_list:
-            if i == "x":
+        # Rellena los 'x' con dígitos aleatorios
+        list_emty = []
+        for i in card:
+            if i == 'x':
                 list_emty.append(str(random.randint(0, 9)))
             else:
                 list_emty.append(i)
+        card = ''.join(list_emty)
 
-        card = "".join(list_emty)
-
-        # VISA/MC/Amex length fix
-        if card[0] == "3":
+        # Longitud según tipo de tarjeta
+        if card[0] == '3':
             card = card[:15]
         else:
             card = card[:16]
 
-        # --- Mes ---
-        if mes in ["xx", "rnd"]:
+        # Generar mes
+        if mes in ['xx', 'rnd']:
             mes_gen = random.randint(1, 12)
             mes_gen = f"{mes_gen:02d}"
         else:
             if int(mes) > 12:
-                return False
+                return []
             mes_gen = mes
 
-        # --- Año ---
-        if ano in ["rnd", "xxxx"]:
+        # Generar año
+        if ano in ['rnd', 'xxxx']:
             ano_gen = random.randint(2023, 2040)
         else:
-            ano_gen = ano
+            ano_gen = int(ano)
             if len(str(ano_gen)) == 2:
-                ano_gen = "20" + str(ano_gen)
+                ano_gen = int("20" + str(ano_gen))
 
-        # --- CVV ---
-        if cvv in ["rnd", "xxx", "xxxx"]:
-            cvv_gen = random.randint(1000, 9999) if card[0] == "3" else random.randint(100, 999)
+        # Generar CVV
+        if cvv in ['rnd', 'xxx', 'xxxx']:
+            cvv_gen = random.randint(1000, 9999) if card[0] == '3' else random.randint(100, 999)
         else:
             cvv = re.findall(r"[0-9]+", cvv)[0]
-            if card[0] in ["4", "5", "6"]:
-                cvv_gen = cvv if len(cvv) >= 3 else (cvv + str(random.randint(100, 999)))[:3]
-            elif card[0] == "3":
-                cvv_gen = cvv if len(cvv) >= 4 else (cvv + str(random.randint(1000, 9999)))[:4]
+            if card[0] == '3':
+                cvv_gen = cvv[:4].zfill(4)
             else:
-                cvv_gen = cvv
+                cvv_gen = cvv[:3].zfill(3)
 
-        # --- Validación con Luhn ---
+        # Validación con Luhn
         if is_luhn_valid(card):
-            IST = pytz.timezone("US/Central")
+            IST = pytz.timezone('US/Central')
             now = datetime.datetime.now(IST)
-            max_reintentos = 10
-            intentos = 0
 
-            while intentos < max_reintentos:
-                intentos += 1
-                if datetime.datetime.strptime(now.strftime("%m-%Y"), "%m-%Y") <= datetime.datetime.strptime(
-                    f"{mes_gen}-{ano_gen}", "%m-%Y"
-                ):
-                    x = f"{card}|{mes_gen}|{ano_gen}|{cvv_gen}\n"
-                    ccs.append(x)
-                    break
-        else:
-            continue
+            if datetime.datetime.strptime(
+                now.strftime("%m-%Y"), "%m-%Y"
+            ) <= datetime.datetime.strptime(f"{mes_gen}-{ano_gen}", "%m-%Y"):
+                x = f"{card}|{mes_gen}|{ano_gen}|{cvv_gen}"
+                ccs.append(x)
 
     return ccs
