2863149a1f9c32ee3f65baa8e80d8b4e7f17d747
BeboCotize <djmalindo25@hotmail.com>
2025-08-27 12:35:13 -0400

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
