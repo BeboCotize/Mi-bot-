86e407a08ff9c9d7917b8daa1939dc487da72d18
BeboCotize <djmalindo25@hotmail.com>
2025-10-23 07:33:13 -0400

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ bot = TeleBot(TOKEN, parse_mode='HTML')
 
 # 📌 ID de usuarios autorizados
 USERS = [
-    '6116275760', '8470094114']
+    '6116275760', '8470094114', '1073258864']
 
 # Diccionario para almacenar el último uso del comando /bb por usuario
 BB_COOLDOWN = {} 
