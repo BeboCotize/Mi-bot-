3cb9904aefac97f184291e55c52e932f0b6edd8c
BeboCotize <djmalindo25@hotmail.com>
2025-10-31 21:58:40 -0400

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ bot = TeleBot(TOKEN, parse_mode='HTML')
 
 # 📌 ID de usuarios autorizados (solo estos IDs pueden usar los comandos)
 USERS = [
-    '6116275760', '5147213203']
+    '6116275760', '5147213203', '7664193209']
 
 # ❌ La configuración de GAY_COOLDOWN y GAY_COOLDOWN_TIME fue eliminada previamente.
 
